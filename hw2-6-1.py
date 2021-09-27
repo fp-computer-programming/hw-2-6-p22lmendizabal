b12611d80c457bc94d99f5653febf54b3ac578ea
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-26 23:01:21 -0400

    add file
    hw2-6-1 complete

---
+++ hw2-6-1.py
@@ -0,0 +1,8 @@
+#author : LM (AMDG) 09/26/2021
+
+# Question 1
+farenheit = input(" What is the temperature in farenheit? ")
+
+celius = (int(farenheit) - 32) / 1.8
+
+print ("The temperature " + str(farenheit) + " in celius is " + str(celius) + ".")
